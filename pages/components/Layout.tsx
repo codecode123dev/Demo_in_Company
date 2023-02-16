ff18904f0a1df5b43da49c0153f034baed808e2b
minh <minhnvgch200213@fpt.edu.vn>
2023-02-16 20:50:34 +0700

    nextjs

---
+++ pages/components/Layout.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+
+const Layout = () =>(
+    // <Head>
+    //     {/* <meta charset="UTF-8"/> */}
+    // <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    // <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    //     <title> 
+    //         My Next App
+    //     </title>
+    //     </Head>
+        
+
+    //     <header>
+    //         <Navbar bg='dark' variant='dark'>
+    //             <Navbar.Brand>
+    //                 My Next app
+    //             </Navbar.Brand>
+    //             <Nav>
+    //                 <Na
+    //             </Nav>
+
+    //         </Navbar>
+    //     </header>
+)
+
+export default Layout
